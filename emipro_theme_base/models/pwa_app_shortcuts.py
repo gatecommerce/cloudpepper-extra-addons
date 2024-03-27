7631da5bbb18e8b653474b03e6dddbcc9e273c6e
gatecommerce <ciro.dambra@gatecommerce.com>
2024-03-27 12:36:39 +0100

    nuovo upgrade sia tema base che clarico a 17.0.0.5

---
+++ emipro_theme_base/models/pwa_app_shortcuts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class PWAAppShortcuts(models.Model):
+    _name = "pwa.app.shortcut.ept"
+
+    name = fields.Char('Name', required=True)
+    description = fields.Char('Description', required=True)
+    url = fields.Char('URL', required=True)
+    icon = fields.Image('Icon', required=True)
+    website_id = fields.Many2one('website', string='Website', ondelete='cascade')
